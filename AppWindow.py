2a7ca33d2803418857a56cf172008b6334faf9fd
GabrielBueno200 <gabrielbueno200@gmail.com>
2022-09-16 16:23:11 -0300

    added import nii.gz and landmarks callbacks

--- AppWindow.py
+++ AppWindow.py
@@ -1,13 +1,12 @@
 import math
 import os
 
-import vtk
-
-import PyQt5.QtWidgets as QtWidgets
 import PyQt5.QtCore as Qt
+import PyQt5.QtWidgets as QtWidgets
+import vtk
 from vtk.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
 
-from reconstruction import setup_skull
+from reconstruction import VtkHandler
 
 
 class AppWindow(QtWidgets.QMainWindow, QtWidgets.QApplication):
@@ -18,7 +17,9 @@ class AppWindow(QtWidgets.QMainWindow, QtWidgets.QApplication):
         self.nii_file_path = "cts.nii.gz"
 
         self.renderer, self.frame, self.vtk_widget, self.interactor, self.render_window = self.setup()
-        self.brain = setup_skull(self.renderer, self.nii_file_path)
+        self.vtk_handler = VtkHandler(self.render_window)
+        self.skull = self.vtk_handler.setup_skull(
+            self.renderer, self.nii_file_path)
 
         self.grid = QtWidgets.QGridLayout()
 
@@ -34,7 +35,6 @@ class AppWindow(QtWidgets.QMainWindow, QtWidgets.QApplication):
         self.set_axial_view()
         self.interactor.Initialize()
         self.show()
-
 
     @staticmethod
     def setup():
@@ -52,20 +52,18 @@ class AppWindow(QtWidgets.QMainWindow, QtWidgets.QApplication):
 
         return renderer, frame, vtk_widget, interactor, render_window
 
-
     def add_vtk_widget(self):
         base_brain_file = os.path.basename(self.nii_file_path)
-
-        base_title = f"Crânio: {base_brain_file}"
 
         wrapper_group_box = QtWidgets.QGroupBox()
         wrapper_layout = QtWidgets.QVBoxLayout()
 
-        vtk_group_box = QtWidgets.QGroupBox(base_title)
+        vtk_group_title = f"Crânio: {base_brain_file}"
+        vtk_group_box = QtWidgets.QGroupBox(vtk_group_title)
         vtk_layout = QtWidgets.QVBoxLayout()
         vtk_layout.addWidget(self.vtk_widget)
         vtk_group_box.setLayout(vtk_layout)
-        
+
         reset_button = QtWidgets.QPushButton("Resetar visualização")
         reset_button.setFixedSize(150, 30)
         reset_button.clicked.connect(lambda _: None)
@@ -78,61 +76,76 @@ class AppWindow(QtWidgets.QMainWindow, QtWidgets.QApplication):
         self.grid.addWidget(wrapper_group_box, 0, 2, 5, 5)
         self.grid.setColumnMinimumWidth(2, 700)
 
-
-    def create_file_selector(self, label, window_title, name_filter):
+    def create_file_selector(self, label, window_title, name_filter, callback):
         file_import_button = QtWidgets.QPushButton(label)
-        file_import_button.clicked.connect(lambda _: self.open_file(window_title, name_filter))
-        
+        file_import_button.clicked.connect(
+            lambda _: self.open_file(window_title, name_filter, callback))
+
         return file_import_button
 
-
-    def open_file(self, window_title, name_filter):
+    def open_file(self, window_title, name_filter, callback):
         dialog = QtWidgets.QFileDialog(self)
         dialog.setWindowTitle(window_title)
         dialog.setNameFilter(name_filter)
         dialog.setDirectory(Qt.QDir.currentPath())
         dialog.setFileMode(QtWidgets.QFileDialog.ExistingFile)
-        dialog.show()
 
+        if dialog.exec_():
+            selected_file = dialog.selectedFiles()[0]
+            callback(selected_file)
 
     def add_skull_settings_widget(self):
         skull_group_box = QtWidgets.QGroupBox("Crânio")
         skull_group_layout = QtWidgets.QGridLayout()
 
-        skull_file_selector = self.create_file_selector("Importar arquivo NIFTI", 'Selecionar arquivo NIFTI', 'Arquivos nii.gz (*.nii.gz)')
+        skull_file_selector = self.create_file_selector(
+            label="Importar arquivo NIFTI",
+            window_title='Selecionar arquivo NIFTI',
+            name_filter='Arquivos nii.gz (*.nii.gz)',
+            callback=lambda file_path: self.vtk_handler.setup_skull(
+                self.renderer, file_path)
+        )
         skull_group_layout.addWidget(skull_file_selector, 1, 0, 1, 3)
 
         skull_group_layout.addWidget(self.create_separator(), 2, 0, 1, 3)
 
-        skull_opacity_slider = self.create_slider(0, 1, self.brain.property.GetOpacity() * 100, self.set_skull_opacity)
+        skull_opacity_slider = self.create_slider(
+            0, 1, self.skull.property.GetOpacity() * 100, self.vtk_handler.set_skull_opacity)
         skull_group_layout.addWidget(QtWidgets.QLabel("Opacidade"), 3, 0)
         skull_group_layout.addWidget(skull_opacity_slider, 3, 1, 1, 2)
 
         skull_group_box.setLayout(skull_group_layout)
         self.grid.addWidget(skull_group_box, 0, 0, 1, 2)
 
-
     def add_landmarks_settings_widget(self):
         landmarks_group_box = QtWidgets.QGroupBox("Pontos Fiduciais")
         landmarks_group_layout = QtWidgets.QGridLayout()
 
-        detect_landmarks_button = QtWidgets.QPushButton("Detectar pontos fiduciais")
-        detect_landmarks_button.clicked.connect(lambda _: None)
+        detect_landmarks_button = QtWidgets.QPushButton(
+            "Detectar pontos fiduciais")
+        detect_landmarks_button.clicked.connect(
+            lambda _: self.vtk_handler.setup_detected_landmarks(self.renderer))
         landmarks_group_layout.addWidget(QtWidgets.QLabel("Automático"), 1, 0)
         landmarks_group_layout.addWidget(detect_landmarks_button, 1, 1)
 
-        landmarks_file_selector = self.create_file_selector("Importar pontos fiduciais", 'Selecionar JSON com pontos fiduciais', 'Arquivos JSON (*.json)')
+        landmarks_file_selector = self.create_file_selector(
+            label="Importar pontos fiduciais",
+            window_title='Selecionar JSON com pontos fiduciais',
+            name_filter='Arquivos JSON (*.json)',
+            callback=lambda file_path: self.vtk_handler.setup_landmarks_from_file(
+                self.renderer, file_path)
+        )
         landmarks_group_layout.addWidget(QtWidgets.QLabel("Manual"), 2, 0)
         landmarks_group_layout.addWidget(landmarks_file_selector, 2, 1)
 
         landmarks_group_layout.addWidget(self.create_separator(), 3, 0, 1, 3)
 
         landmarks_visible_checkbox = QtWidgets.QCheckBox("Visível")
-        landmarks_group_layout.addWidget(landmarks_visible_checkbox, 4, 0, 1, 3)
+        landmarks_group_layout.addWidget(
+            landmarks_visible_checkbox, 4, 0, 1, 3)
 
         landmarks_group_box.setLayout(landmarks_group_layout)
         self.grid.addWidget(landmarks_group_box, 1, 0, 2, 2)
-
 
     def add_views_widget(self):
         axial_view = QtWidgets.QPushButton("Axial")
@@ -147,15 +160,59 @@ class AppWindow(QtWidgets.QMainWindow, QtWidgets.QApplication):
 
         views_box.setLayout(views_box_layout)
         self.grid.addWidget(views_box, 3, 0, 2, 2)
-        
+
         axial_view.clicked.connect(self.set_axial_view)
         coronal_view.clicked.connect(self.set_coronal_view)
         sagittal_view.clicked.connect(self.set_sagittal_view)
 
-    
+    def set_axial_view(self):
+        self.renderer.ResetCamera()
+        focal_point = self.renderer.GetActiveCamera().GetFocalPoint()
+        position = self.renderer.GetActiveCamera().GetPosition()
+        distance = math.sqrt((position[0] - focal_point[0]) ** 2 + (
+            position[1] - focal_point[1]) ** 2 + (position[2] - focal_point[2]) ** 2)
+        self.renderer.GetActiveCamera().SetPosition(
+            focal_point[0], focal_point[1], focal_point[2] + distance)
+        self.renderer.GetActiveCamera().SetViewUp(0.0, 1.0, 0.0)
+        self.renderer.GetActiveCamera().Zoom(1.8)
+        self.render_window.Render()
+
+    def set_coronal_view(self):
+        self.renderer.ResetCamera()
+        focal_point = self.renderer.GetActiveCamera().GetFocalPoint()
+        position = self.renderer.GetActiveCamera().GetPosition()
+        distance = math.sqrt((position[0] - focal_point[0]) ** 2 + (
+            position[1] - focal_point[1]) ** 2 + (position[2] - focal_point[2]) ** 2)
+        self.renderer.GetActiveCamera().SetPosition(
+            focal_point[0], focal_point[2] - distance, focal_point[1])
+        self.renderer.GetActiveCamera().SetViewUp(0.0, 0.5, 0.5)
+        self.renderer.GetActiveCamera().Zoom(1.8)
+        self.render_window.Render()
+
+    def set_sagittal_view(self):
+        self.renderer.ResetCamera()
+        focal_point = self.renderer.GetActiveCamera().GetFocalPoint()
+        position = self.renderer.GetActiveCamera().GetPosition()
+        distance = math.sqrt((position[0] - focal_point[0]) ** 2 + (
+            position[1] - focal_point[1]) ** 2 + (position[2] - focal_point[2]) ** 2)
+        self.renderer.GetActiveCamera().SetPosition(
+            focal_point[2] + distance, focal_point[0], focal_point[1])
+        self.renderer.GetActiveCamera().SetViewUp(0.0, 0.0, 1.0)
+        self.renderer.GetActiveCamera().Zoom(1.6)
+        self.render_window.Render()
+
+    def create_separator(self):
+        separator = QtWidgets.QWidget()
+        separator.setFixedHeight(1)
+        separator.setSizePolicy(
+            QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
+        separator.setStyleSheet("background-color: #c8c8c8;")
+
+        return separator
+
     def create_slider(self, min_value, max_value, initial_value, change_callback):
         slider = QtWidgets.QSlider(Qt.Qt.Horizontal)
-        slider.setValue(initial_value)
+        slider.setValue(int(initial_value))
 
         slider.valueChanged.connect(lambda _: change_callback(slider.value()))
 
@@ -163,50 +220,3 @@ class AppWindow(QtWidgets.QMainWindow, QtWidgets.QApplication):
         slider.setMaximum(max_value * 100)
 
         return slider
-
-
-    def set_skull_opacity(self, opacity_value):
-        self.brain.property.SetOpacity(opacity_value / 100)
-        self.render_window.Render()
-
-
-    def set_axial_view(self):
-        self.renderer.ResetCamera()
-        focal_point = self.renderer.GetActiveCamera().GetFocalPoint()
-        position = self.renderer.GetActiveCamera().GetPosition()
-        distance = math.sqrt((position[0] - focal_point[0]) ** 2 + (position[1] - focal_point[1]) ** 2 + (position[2] - focal_point[2]) ** 2)
-        self.renderer.GetActiveCamera().SetPosition(focal_point[0], focal_point[1], focal_point[2] + distance)
-        self.renderer.GetActiveCamera().SetViewUp(0.0, 1.0, 0.0)
-        self.renderer.GetActiveCamera().Zoom(1.8)
-        self.render_window.Render()
-
-
-    def set_coronal_view(self):
-        self.renderer.ResetCamera()
-        focal_point = self.renderer.GetActiveCamera().GetFocalPoint()
-        position = self.renderer.GetActiveCamera().GetPosition()
-        distance = math.sqrt((position[0] - focal_point[0]) ** 2 + (position[1] - focal_point[1]) ** 2 + (position[2] - focal_point[2]) ** 2)
-        self.renderer.GetActiveCamera().SetPosition(focal_point[0], focal_point[2] - distance, focal_point[1])
-        self.renderer.GetActiveCamera().SetViewUp(0.0, 0.5, 0.5)
-        self.renderer.GetActiveCamera().Zoom(1.8)
-        self.render_window.Render()
-
-
-    def set_sagittal_view(self):
-        self.renderer.ResetCamera()
-        focal_point = self.renderer.GetActiveCamera().GetFocalPoint()
-        position = self.renderer.GetActiveCamera().GetPosition()
-        distance = math.sqrt((position[0] - focal_point[0]) ** 2 + (position[1] - focal_point[1]) ** 2 + (position[2] - focal_point[2]) ** 2)
-        self.renderer.GetActiveCamera().SetPosition(focal_point[2] + distance, focal_point[0], focal_point[1])
-        self.renderer.GetActiveCamera().SetViewUp(0.0, 0.0, 1.0)
-        self.renderer.GetActiveCamera().Zoom(1.6)
-        self.render_window.Render()
-
-
-    def create_separator(self):
-        separator = QtWidgets.QWidget()
-        separator.setFixedHeight(1)
-        separator.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Fixed)
-        separator.setStyleSheet("background-color: #c8c8c8;")
-        
-        return separator
--- reconstruction.py
+++ reconstruction.py
@@ -1,69 +1,188 @@
+import json
+
 import vtk
 
-class VtkShape:
+
+class VtkVolume:
     def __init__(self):
         self.reader = None
         self.property = None
 
-def read_volume(file_name):
-    reader = vtk.vtkNIFTIImageReader()
-    reader.SetFileNameSliceOffset(1)
-    reader.SetDataByteOrderToBigEndian()
-    reader.SetFileName(file_name)
-    reader.Update()
 
-    # reader
-    reader = vtk.vtkNIFTIImageReader()
-    reader.SetFileName(file_name)
-    reader.Update()
+class VtkHandler:
+    _skull = VtkVolume()
+    _detected_landmarks = VtkVolume()
+    _real_landmarks = VtkVolume()
+    _colors = vtk.vtkNamedColors()
+    render_window: vtk.vtkRenderWindow
 
-    # volume
-    volume = vtk.vtkImageData()
-    volume.DeepCopy(reader.GetOutput())
+    def __init__(self, render_window):
+        self.render_window = render_window
 
-    # surface
-    surface = vtk.vtkFlyingEdges3D()
-    surface.SetInputData(volume)
-    surface.ComputeNormalsOn()
-    surface.SetValue(0, 100)
+    def _point_to_glyph(self, points):
+        bounds = points.GetBounds()
+        max_len = 0
+        for i in range(1, 3):
+            max_len = max(bounds[i + 1] - bounds[i], max_len)
 
-    # transformations
-    coordinates_transform = vtk.vtkTransform()
-    coordinates_transform.SetMatrix(reader.GetQFormMatrix())
+        sphere_source = vtk.vtkSphereSource()
+        sphere_source.SetRadius(2)
 
-    transform_filter = vtk.vtkTransformFilter()
-    transform_filter.SetTransform(coordinates_transform)
-    transform_filter.SetInputConnection(surface.GetOutputPort())
-    transform_filter.Update()
+        polydata = vtk.vtkPolyData()
+        polydata.SetPoints(points)
 
-    # mapper
-    mapper = vtk.vtkPolyDataMapper()
-    mapper.SetInputConnection(transform_filter.GetOutputPort())
-    mapper.ScalarVisibilityOff()
+        mapper = vtk.vtkGlyph3DMapper()
+        mapper.SetInputData(polydata)
+        mapper.SetSourceConnection(sphere_source.GetOutputPort())
+        mapper.ScalarVisibilityOff()
+        mapper.ScalingOff()
 
-    # actor
-    actor = vtk.vtkActor()
-    actor.SetMapper(mapper)
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
 
-    # property
-    colors = vtk.vtkNamedColors()
+        return actor
 
-    property = vtk.vtkProperty()
-    property.SetColor(colors.GetColor3d('Green'))
-    property.SetOpacity(1)
+    def _reconstruct_skull(self, file_name):
+        reader = vtk.vtkNIFTIImageReader()
+        reader.SetFileNameSliceOffset(1)
+        reader.SetDataByteOrderToBigEndian()
+        reader.SetFileName(file_name)
+        reader.Update()
 
-    actor.SetProperty(property)
+        reader = vtk.vtkNIFTIImageReader()
+        reader.SetFileName(file_name)
+        reader.Update()
 
-    return actor, reader, property
+        volume = vtk.vtkImageData()
+        volume.DeepCopy(reader.GetOutput())
 
+        surface = vtk.vtkFlyingEdges3D()
+        surface.SetInputData(volume)
+        surface.ComputeNormalsOn()
+        surface.SetValue(0, 100)
 
-def setup_skull(renderer, file):
-    skull = VtkShape()
-    actor, reader, property = read_volume(file) 
+        coordinates_transform = vtk.vtkTransform()
+        coordinates_transform.SetMatrix(reader.GetQFormMatrix())
 
-    skull.reader = reader
-    skull.property = property
+        transform_filter = vtk.vtkTransformFilter()
+        transform_filter.SetTransform(coordinates_transform)
+        transform_filter.SetInputConnection(surface.GetOutputPort())
+        transform_filter.Update()
 
-    renderer.AddActor(actor) 
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(transform_filter.GetOutputPort())
+        mapper.ScalarVisibilityOff()
 
-    return skull
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+
+        property = vtk.vtkProperty()
+        property.SetColor(self._colors.GetColor3d('Green'))
+        property.SetOpacity(1)
+
+        actor.SetProperty(property)
+
+        return actor, reader, property
+
+    def _get_landmarks_shape(self, lps_landmarks, plot_color):
+        ras_landmarks = self._get_ras_landmarks_points(lps_landmarks)
+
+        landmark_transform = vtk.vtkLandmarkTransform()
+        landmark_transform.SetSourceLandmarks(ras_landmarks)
+
+        polydata = vtk.vtkPolyData()
+        polydata.SetPoints(ras_landmarks)
+
+        glyph_filter = vtk.vtkVertexGlyphFilter()
+        glyph_filter.SetInputData(polydata)
+        glyph_filter.Update()
+
+        transform_filter = vtk.vtkTransformPolyDataFilter()
+        transform_filter.SetInputConnection(glyph_filter.GetOutputPort())
+        transform_filter.SetTransform(landmark_transform)
+        transform_filter.Update()
+
+        actor = self._point_to_glyph(glyph_filter.GetOutput().GetPoints())
+
+        property = vtk.vtkProperty()
+        property.SetColor(self._colors.GetColor3d(plot_color))
+        property.SetOpacity(1)
+
+        actor.SetProperty(property)
+
+        return actor, property
+
+    def _convert_landmarks_to_ras_coordinates(self, lps_landmarks):
+        for lps_landmark in lps_landmarks:
+            yield [-lps_landmark[0], -lps_landmark[1], lps_landmark[2]]
+
+    def _get_ras_landmarks_points(self, lps_landmarks):
+        points = vtk.vtkPoints()
+
+        ras_landmarks = self._convert_landmarks_to_ras_coordinates(
+            lps_landmarks)
+
+        [points.InsertNextPoint(landmark) for landmark in ras_landmarks]
+
+        return points
+
+    def _load_landmarks_from_file(self, json_file_path):
+        with open(json_file_path, 'r') as json_file:
+            data = json.load(json_file)
+            lps_landmarks = [landmark['position']
+                             for landmark in data['markups'][0]['controlPoints']]
+
+            print(lps_landmarks)
+
+        return lps_landmarks
+
+    def _detect_landmarks(self):
+        real_landmarks = [[-41.37749481201172, -71.82634735107422, 14.915303230285645], [-35.485530853271484, -72.81812286376953, -5.548721790313721], [7.642332553863525, -93.41370391845703, 15.821390151977539], [54.65541458129883, -71.99036407470703, -4.924007415771484], [58.362796783447266, -74.64672088623047, 14.933220863342285], [62.50477981567383, -69.75077056884766, 20.013507843017578], [-45.34592819213867, -68.8386001586914, 20.963350296020508], [5.025712490081787, -106.49911499023438, -2.1380300521850586], [0.4708743393421173,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         -4.421438217163086, 115.38072204589844]]
+
+        detected_landmarks = [[-41.37749481201172 + 6, -71.82634735107422, 14.915303230285645], [-35.485530853271484 + 6, -72.81812286376953, -5.548721790313721], [7.642332553863525 + 6, -93.41370391845703, 15.821390151977539], [54.65541458129883 + 6, -71.99036407470703, -4.924007415771484], [58.362796783447266 + 6, -74.64672088623047, 14.933220863342285], [62.50477981567383 + 6, -69.75077056884766, 20.013507843017578], [-45.34592819213867 + 6, -68.8386001586914, 20.963350296020508], [5.025712490081787 + 6, -106.49911499023438, -2.1380300521850586], [0.4708743393421173 + 6,
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             -4.421438217163086, 115.38072204589844]]
+
+        return real_landmarks, detected_landmarks
+
+    def set_skull_opacity(self, opacity_value):
+        self._skull.property.SetOpacity(opacity_value / 100)
+        self.render_window.Render()
+
+    def setup_detected_landmarks(self, renderer):
+        real_landmarks, detected_landmarks = self._detect_landmarks()
+
+        real_landmarks_actor, real_landmarks_props = self._get_landmarks_shape(
+            real_landmarks, "tomato")
+        detected_landmarks_actor, detected_landmarks_props = self._get_landmarks_shape(
+            detected_landmarks, "blue")
+
+        self._detected_landmarks.property = detected_landmarks_props
+        self._real_landmarks.property = real_landmarks_props
+
+        renderer.AddActor(detected_landmarks_actor)
+        renderer.AddActor(real_landmarks_actor)
+
+        return self._real_landmarks, self._detected_landmarks
+
+    def setup_landmarks_from_file(self, renderer, json_file_path):
+        real_landmarks = self._load_landmarks_from_file(json_file_path)
+
+        real_landmarks_actor, real_landmarks_props = self._get_landmarks_shape(
+            real_landmarks, "tomato")
+
+        self._real_landmarks.property = real_landmarks_props
+
+        renderer.AddActor(real_landmarks_actor)
+
+        return self._real_landmarks
+
+    def setup_skull(self, renderer, file_path):
+        actor, reader, property = self._reconstruct_skull(file_path)
+
+        self._skull.reader = reader
+        self._skull.property = property
+
+        renderer.AddActor(actor)
+
+        return self._skull
