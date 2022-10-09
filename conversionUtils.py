90587c6585686b4ad048d87af5e42e633de8e0f8
Antonio Muniz <antonio.gustavo_@live.com>
2022-10-09 18:55:33 -0300

    Update conversionUtils.py

--- conversionUtils.py
+++ conversionUtils.py
@@ -5,6 +5,7 @@ import dicom2nifti
 
 
 def get_nifti_from_dicom_series(path_to_dicom_series):
+    dicom2nifti.settings.disable_validate_orthogonal()
     dicom2nifti.dicom_series_to_nifti(
         path_to_dicom_series,
         os.path.join(os.getcwd(), 'patient.nii.gz')
